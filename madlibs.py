aab1e4b6c7637c00991f90b3fbf5fd4b56bb8573
Kariuki11 <irungukenneth433@gmail.com>
2024-07-03 02:01:16 +0300

    New commits on some small python projects

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+#String Concatenation (how to put strings together)
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib ="Computer programming is so {adj}! It makes me excited all the time because \ I love to {verb1}. Stay hydrated and {verb2} Like you are {famous_person}!"
+
+print(madlib)
