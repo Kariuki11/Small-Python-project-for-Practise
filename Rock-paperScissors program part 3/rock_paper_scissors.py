a2e4464f3c4486aec308d7ab6b3ef4d10275a46b
Kariuki11 <irungukenneth433@gmail.com>
2024-07-03 03:32:33 +0300

    commits

--- Rock-paperScissors program part 3/rock_paper_scissors.py
+++ Rock-paperScissors program part 3/rock_paper_scissors.py
@@ -18,3 +18,6 @@ def is_win(player, opponent):
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
         or (player == 'p' and opponent == 'r'):
             return True
+        
+        
+1:10min 
